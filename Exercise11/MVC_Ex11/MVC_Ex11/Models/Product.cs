daed7fb94d9b5feedf5eca9b9a4cad74c00c0dc9
JavaJoseph <joseph.ar.ogren@outlook.com>
2024-04-16 09:30:10 +0200

    created Project MVC

---
+++ Exercise11/MVC_Ex11/MVC_Ex11/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿namespace MVC_Ex11.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ProductId { get; set; } = Guid.NewGuid().ToString();
+
+        public string ProductName { get; set; } = string.Empty;
+
+        public string Category { get; set; }
+
+        public string Shelf { get; set; }
+
+
+        public int Price { get; set; }        
+
+        public int Count { get; set; }  
+
+
+        // CONSTRUCTOR 
+        public Product() 
+        {
+            Console.WriteLine("new Product created!");
+        }
+
+                    
+    }
+}
